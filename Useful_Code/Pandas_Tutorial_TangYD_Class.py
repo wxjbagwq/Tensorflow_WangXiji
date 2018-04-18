873b53559767b1406703976ae015371c257ba644
wxjbagwq <34207833+wxjbagwq@users.noreply.github.com>
2018-04-18 22:47:19 +0800

    Update Pandas_Tutorial_TangYD_Class.py

--- Useful_Code/Pandas_Tutorial_TangYD_Class.py
+++ Useful_Code/Pandas_Tutorial_TangYD_Class.py
@@ -63,7 +63,7 @@ max_calories = food_info["Energ_Kcal"].max()
 nomalized_calories = food_info["Energ_Kcal"] / max_calories                      # 归一化
 nomalized_protein  = food_info["Protein_(g)"] / food_info["Protein_(g)"].max()   # 归一化
 
-# 排序
+# 排序.sort_values()
 food_info.sort_values("Sodium_(mg)", inplace=True)                      # 对Sodium_(mg)列进行排序，inplace表示排序好的列是替换 
                                                                         # 原列还是变成新列加入数据集。默认是从小到大的排列
 food_info.sort_values("Sodium_(mg)", inplace=True, ascending=False)     # ascending=False表示降序排列。NaN表示缺失值，放在最后                                                                
@@ -110,25 +110,126 @@ port_stats = titanic_survival.pivot_table(index="Embarked", values=["Fare","Surv
 drop_na_columns = titanic_survival.dropna(axis=1)                               # axis=1/0按列/行丢弃
 new_titanic_survival = titanic_survival.dropna(axis=0, subset=["Age","Sex"])    # 丢弃"Age"和"Sex"为缺失值的行！注意这里是行！
 
-# 定位到具体的值                             [行数， 列数]
+# 定位到具体的值.loc                         [行数， 列数]
 row_index_83_age      = titanic_survival.loc[83, "Age"]              # 找到第83个样本的"Age"值
 row_index_1000_pclass = titanic_survival.loc[76, "Pclass"]           # 找到第76个样本的"Pclass"值
   
+# 重新生成索引.reset_index()
+new_titanic_survival = titanic_survival.sort_values("Age"m ascengding=False)        # "Age"列降序排序
+print(new_titanic_survival[0:10])                                                   # 打印这0-10个，结果正确但是索引却是原始的索引
+titanic_reindexed = new_titanic_survival.reset_index(drop=True)                     # .reset_index可以重新生成索引
+print('-----------------')
+print(titanic_reindexed.loc[0:10])
   
+# Pandas自定义函数.apply()
+def hundredth_row(column):
+  hundredth_item = column.loc[99]
+  return hundredth_item
+hundredth_row = titanic_survival.apply(hundredth_row)
+print(hundredth_row)
+
+def not_null_count(column):
+  column_null = pd.isnull(column)
+  null = column[column_null]
+  return null
+column_null_count = titanic_survival.apply(not_null_count)
+print(column_null_count)
+
+def which_class(row):
+  pclass = row['Pclass']
+  if pd.isnull(pclass):
+    return "Unkown"
+  elif pclass == 1:
+    return "First Class"
+  elif pclass == 2:
+    return "Second Class"
+  elif pclass == 3:
+    return "Third Class"
+classes = titanic_survival.apply(which_class, axis=1)            # axis = 1 --> 按列处理
+print(classes)
   
+def is_minor(row):
+  if row["Age"] < 18:
+    return True
+  else:
+    return False
+minors = titanic_survival.apply(is_minor, axis=1)
+print(minors)
+
+def generate_age_label(row):
+  age = row["Age"]
+  if pd.isnull(age):
+    return "unknown"
+  elif age < 18:
+    return "minor"
+  else:
+    reutnr "adult"
+age_labels = titanic_survival.apply(generate_age_label, axis=1)
+print(age_labels)
+
+# 获救的人是否成年的比例
+titanic_survival['age_labels'] = age_labels
+age_group_survival = titanic_survival.pivot_table(index="age_labels", values="Survived")
+print(age_group_survival)
+###  
   
+# Series结构
+###
+# Series结构是DataFrame(经过read_csv得到的)其中的一行或者一列
+# DataFrame由Series构成；Series由np.array构成
+###  
+import pandas as pd
+fandango    = pd.read_csv('fandango_score_comparison.csv')       # 这个数据集是对一些列电影的不同媒体的评分等等构成的
+series_film = fandango['FILM'] 
+print(type(series_film))
+print(series_film[0:5])
+series_rt = fandango['RottenTomatoes']
+print(series_rt[0:5])
+
+from pandas import Series
+film_names = series_film.values
+rt_scores = series_rt.values
+series_custom = Series(rt_scores, index=film_names)
+series_custom[['Minions (2015)', 'Leviathan (2014)']]
+fiveten = series_custom[5:10]  
+
+original_index = series_custom.index.tolist()
+sorted_index = sorted(original_index)
+sorted_by_index = series_custom.reindex(sorted_index)
   
+sc2 = series_custom.sort_index()
+sc3 = series_custom.sort_values()
+print(sc3[0:10])
+
+import numpy as np
+print(np.add(series_custom, series_custom))
+np.sin(series_custom)
+np.max(series_custom)
   
+series_custom > 50
+series_greater_than_50 = series_custom[series_custom > 50]
+criteria_one  = series_custom > 50
+criteria_two  = series_custom < 75
+both_criteria = series_custom[criteria_one & criteria_two] 
+print(both_criteria)
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+rt_critics = Series(fandango['RottenTomatoes'].values, index=fandango['FILM'])     
+rt_users   = Series(fandango['RottenTomatoes_User'].values, index=fandango['FILM'])
+rt_mean    = (rt_critics + rt_users) / 2
+print(rt_mean) 
 ###  
 
+###
+# 做一个Series结构
+###
+import pandas as pd
+
+fandango = pd.read_csv('fandango_score_comparison.csv')
+print(type(fandango))
+fandango_films = fandango.set_index('FILM', drop=False)
+
+fandango_films["Avengers: Age of Ultron (2015)" : "Hot Tub Time Mahine 2 (2015)"]
+fandango_films.loc["Avengers: Age of Ultron (2015)" : "Hot Tub Time Mahine 2 (2015)"]
+fandango_films.loc['Kumiko, The Treasure Hunter (2015)']
+movies = ['Kumiko, The Treasure Hunter (2015)' , 'Do You Believe? (2015)' , 'Ant-Man (2015)']
+###
